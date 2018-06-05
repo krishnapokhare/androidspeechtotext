6b5a389ea4af45d86b8763f37af86cc6a3c07a8c
krishnapokhare@gmail.com <tigers@1234>
2018-06-05 14:25:58 -0700

    Updated the Permissions functionality for MainActivity.java

--- app/src/main/java/com/kpokhare/offlinespeechrecognizer/MainActivity.java
+++ app/src/main/java/com/kpokhare/offlinespeechrecognizer/MainActivity.java
@@ -1,8 +1,10 @@
 package com.kpokhare.offlinespeechrecognizer;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,7 +14,10 @@ import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.ActionMode;
@@ -41,6 +46,7 @@ import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends BaseActivity implements RecognitionListener {
+    private static final int REQUEST_RECORD_PERMISSION = 100;
     private static final String MINIMUM_SPEECH_INTERVAL = "15";
     private static final String SILENCE_LENGTH = "5";
     private static final int MENU_PLAY = 1000;
@@ -50,7 +56,6 @@ public class MainActivity extends BaseActivity implements RecognitionListener {
     private int WordCountIntervalIncrementor = WordCountInterval;
     private SpeechRecognizer speech;
     private Intent recognizerIntent;
-    //    private SharedPreferences preferences;
     private String recordingLangCode;
     private TextView speechTextView;
     private String finalResult;
@@ -62,8 +67,6 @@ public class MainActivity extends BaseActivity implements RecognitionListener {
     private TextView speakingLangHeadingTextView;
     private TextToSpeech textToSpeech;
     private boolean readyToSpeak;
-    //    private ActionMode.Callback mActionModeCallback;
-//    private Object mActionMode;
     private String keyword;
     private Date intervalSpeechStopDate;
     private Date intervalSpeechStartDate;
@@ -73,9 +76,6 @@ public class MainActivity extends BaseActivity implements RecognitionListener {
     private TextView errorTextView;
     private int startSpeechMessageCount;
     private TextView keywordValueTextView;
-//    private File recordingFile;
-//    private boolean isPlaying;
-//    private MediaRecorder myAudioRecorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,75 +98,9 @@ public class MainActivity extends BaseActivity implements RecognitionListener {
 
         languages = getResources().getStringArray(R.array.languages);
         languageValues = getResources().getStringArray(R.array.languages_values);
-//        Log.d(LOG_TAG_DEBUG, "onCreate: languages" + languages.length);
-//        Log.d(LOG_TAG_DEBUG, "onCreate: languageValues" + languageValues.length);
 
         PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
         new LoadSupportedLanguagesTask(this).execute("test");
-//
-//        File path = new File(
-//                Environment.getExternalStorageDirectory().getAbsolutePath()
-//                        + "/Android/data/com.kpokhare.offlinespeechrecognizer/files/");
-//        Log.i(LOG_TAG_DEBUG,path.getAbsolutePath());
-//        path.mkdirs();
-//        try {
-//            recordingFile = File.createTempFile("recording", ".3gp", path);
-//        } catch (IOException e) {
-//            throw new RuntimeException("Couldn't create file on SD card", e);
-//        }
-
-//        mActionModeCallback = new ActionMode.Callback() {
-//
-//            // Called when the action mode is created; startActionMode() was called
-//            @Override
-//            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-//                // Inflate a menu resource providing context menu items
-//                MenuInflater inflater = mode.getMenuInflater();
-//                inflater.inflate(R.menu.context_menu, menu);
-//                return true;
-//            }
-//
-//            // Called each time the action mode is shown. Always called after onCreateActionMode, but
-//            // may be called multiple times if the mode is invalidated.
-//            @Override
-//            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-//                return false; // Return false if nothing is done
-//            }
-//
-//            // Called when the user selects a contextual menu item
-//            @Override
-//            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-//                switch (item.getItemId()) {
-//                    case R.id.action_play:
-//                        Log.i(LOG_TAG_DEBUG,"Play clicked");
-//                        mode.finish(); // Action picked, so close the CAB
-//                        return true;
-//                    default:
-//                        return false;
-//                }
-//            }
-//
-//            // Called when the user exits the action mode
-//            @Override
-//            public void onDestroyActionMode(ActionMode mode) {
-//                mActionMode = null;
-//            }
-//        };
-//
-//        speechTextView.setOnLongClickListener(new View.OnLongClickListener() {
-//            // Called when the user long-clicks on someView
-//            public boolean onLongClick(View view) {
-//                Log.d(LOG_TAG_DEBUG,"Textview Long Clicked");
-//                if (mActionMode != null) {
-//                    return false;
-//                }
-//
-//                // Start the CAB using the ActionMode.Callback defined above
-//                mActionMode = view.startActionMode(mActionModeCallback);
-//                view.setSelected(true);
-//                return true;
-//            }
-//        });
 
         speechTextView.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
             @Override
@@ -201,7 +135,6 @@ public class MainActivity extends BaseActivity implements RecognitionListener {
                     // Perform your definition lookup with the selected text
                     final CharSequence selectedText = speechTextView.getText().subSequence(min, max);
                     PlayTextToSpeech(selectedText.toString());
-                    Log.d(LOG_TAG_DEBUG, "selected text is " + selectedText);
                     // Finish and close the ActionMode
                     mode.finish();
                     return true;
@@ -216,9 +149,25 @@ public class MainActivity extends BaseActivity implements RecognitionListener {
         });
     }
 
+    private boolean checkRecordingPermission() {
+        Log.d(LOG_TAG_DEBUG, "Method: CheckRecordingPermission");
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions
+                    (MainActivity.this,
+                            new String[]{Manifest.permission.RECORD_AUDIO},
+                            REQUEST_RECORD_PERMISSION);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     private void onRecordingButtonClick() {
         if (!isRecording) {
-            onStartButtonClick();
+            if (checkRecordingPermission()) {
+                onStartButtonClick();
+            }
         } else {
             onStopButtonClick();
         }
@@ -327,10 +276,6 @@ public class MainActivity extends BaseActivity implements RecognitionListener {
             onStopButtonClick();
             Toast.makeText(this, "Error: " + getErrorText(error), Toast.LENGTH_SHORT).show();
         }
-//        else{
-//            Snackbar.make(findViewById(R.id.mainCoordinatorLayout), "Error:" + getErrorText(error), Snackbar.LENGTH_SHORT).show();
-//            onStopButtonClick();
-//        }
     }
 
     @Override
@@ -537,26 +482,6 @@ public class MainActivity extends BaseActivity implements RecognitionListener {
         }
     }
 
-//    private void CalculateKeywordCount(String recordingText) {
-//        Log.d(LOG_TAG_DEBUG, "Method: CalculateKeywordCount");
-//        Log.d(LOG_TAG_DEBUG, "keyword: " + keyword);
-//        Log.d(LOG_TAG_DEBUG, "Final Result: " + recordingText);
-//        if (keyword != null) {
-//            TextView keywordTextView = findViewById(R.id.keywordTextView);
-//            keywordTextView.setText(getString(R.string.keyword_count_text) + Global.CountOfSubstringInString(recordingText, keyword));
-//        }
-//    }
-
-//    private void CalculateAvgWordCount(long timeTaken, String words) {
-//        Log.d(LOG_TAG_DEBUG, "Method: CalculateAvgWordCount");
-//        int wordCount = Global.countWordsUsingSplit(words);
-//        Log.d(LOG_TAG_DEBUG, "Word Count:" + Integer.toString(wordCount));
-//        avgWordCount = wordCount / (timeTaken / WordCountInterval);
-//        Log.d(LOG_TAG_DEBUG, "Avg Word Count:" + Long.toString(avgWordCount));
-//        WordCountIntervalIncrementor = WordCountIntervalIncrementor + WordCountInterval;
-//        wordCountTextView.setText("Status:" + Long.toString(avgWordCount) + " words per " + Integer.toString(WordCountInterval) + " seconds.");
-//    }
-
     @Override
     protected void onStop() {
         super.onStop();
@@ -741,127 +666,20 @@ public class MainActivity extends BaseActivity implements RecognitionListener {
             Log.w(LOG_TAG_DEBUG, "No text present for speech");
         }
     }
-//
-//    int frequency = 11025,channelConfiguration = AudioFormat.CHANNEL_IN_MONO;
-//    int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
-//
-//    private class RecordAudio1 extends AsyncTask<Void, Integer, Void> {
-//        @Override
-//        protected Void doInBackground(Void... params) {
-//            isRecording = true;
-//            try {
-//                DataOutputStream dos = new DataOutputStream(
-//                        new BufferedOutputStream(new FileOutputStream(
-//                                recordingFile)));
-//                int bufferSize = AudioRecord.getMinBufferSize(frequency,
-//                        channelConfiguration, audioEncoding);
-//                AudioRecord audioRecord = new AudioRecord(
-//                        MediaRecorder.AudioSource.MIC, frequency,
-//                        channelConfiguration, audioEncoding, bufferSize);
-//
-//                short[] buffer = new short[bufferSize];
-//                audioRecord.startRecording();
-//                int r = 0;
-//                while (isRecording) {
-//                    int bufferReadResult = audioRecord.read(buffer, 0,
-//                            bufferSize);
-//                    for (int i = 0; i < bufferReadResult; i++) {
-//                        dos.writeShort(buffer[i]);
-//                    }
-//                    publishProgress(new Integer(r));
-//                    r++;
-//                }
-//                audioRecord.stop();
-//                dos.close();
-//            } catch (Throwable t) {
-//                Log.e("AudioRecord", "Recording Failed");
-//            }
-//            return null;
-//        }
-//        protected void onProgressUpdate(Integer... progress) {
-//            //statusText.setText(progress[0].toString());
-//        }
-//        protected void onPostExecute(Void result) {
-//            //startRecordingButton.setEnabled(true);
-//            //stopRecordingButton.setEnabled(false);
-//            //startPlaybackButton.setEnabled(true);
-//            Toast.makeText(MainActivity.this, "Recording completed", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//    private void InitializeRecording(){
-//        myAudioRecorder = new MediaRecorder();
-//        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-//        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-//        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-//        myAudioRecorder.setOutputFile(recordingFile);
-//    }
-//
-//    private void StopRecording(){
-//        if(myAudioRecorder != null) {
-//            myAudioRecorder.stop();
-//            myAudioRecorder.release();
-//            myAudioRecorder = null;
-//        }
-//    }
-//
-//    private class RecordAudio extends AsyncTask<Void, Integer, Void> {
-//        @Override
-//        protected Void doInBackground(Void... params) {
-//            isRecording = true;
-//            try {
-//                myAudioRecorder.prepare();
-//                myAudioRecorder.start();
-//            } catch (IllegalStateException ise) {
-//                ise.printStackTrace();
-//            } catch (IOException ioe) {
-//                ioe.printStackTrace();
-//            }
-//            return null;
-//        }
-//        protected void onProgressUpdate(Integer... progress) {
-//            //statusText.setText(progress[0].toString());
-//        }
-//        protected void onPostExecute(Void result) {
-//            //startRecordingButton.setEnabled(true);
-//            //stopRecordingButton.setEnabled(false);
-//            //startPlaybackButton.setEnabled(true);
-//            Toast.makeText(MainActivity.this, "Recording completed", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//    private class PlayAudio extends AsyncTask<Void, Integer, Void> {
-//        @Override
-//        protected Void doInBackground(Void... params) {
-//            isPlaying = true;
-//
-//            int bufferSize = AudioTrack.getMinBufferSize(frequency,channelConfiguration, audioEncoding);
-//            short[] audiodata = new short[bufferSize / 4];
-//
-//            try {
-//                Log.i(LOG_TAG_DEBUG,recordingFile.getAbsolutePath());
-//                DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(recordingFile)));
-//                AudioTrack audioTrack = new AudioTrack(
-//                        AudioManager.STREAM_MUSIC, frequency,
-//                        channelConfiguration, audioEncoding, bufferSize,
-//                        AudioTrack.MODE_STREAM);
-//
-//                audioTrack.play();
-//                while (isPlaying && dis.available() > 0) {
-//                    int i = 0;
-//                    while (dis.available() > 0 && i < audiodata.length) {
-//                        audiodata[i] = dis.readShort();
-//                        i++;
-//                    }
-//                    audioTrack.write(audiodata, 0, audiodata.length);
-//                }
-//                dis.close();
-////                startPlaybackButton.setEnabled(false);
-////                stopPlaybackButton.setEnabled(true);
-//            } catch (Throwable t) {
-//                Log.e("AudioTrack", "Playback Failed");
-//            }
-//            return null;
-//        }
-//    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(LOG_TAG_DEBUG, "Method: onRequestPermissionsResult");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(LOG_TAG_DEBUG, "Permission Granted");
+                    onStartButtonClick();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied! You won't be able to use Recording function of this app.", Toast
+                            .LENGTH_SHORT).show();
+                }
+        }
+    }
 }
